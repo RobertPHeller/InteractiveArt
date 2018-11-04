c8b249dc17317e0f399ca1b59f2a0ea4177cbc69
Robert Heller <heller@deepsoft.com>
2018-11-04 10:03:31 -0500

    Completed SteelWheels

--- SteelWheels/SteelWheels/SteelWheels.ino
+++ SteelWheels/SteelWheels/SteelWheels.ino
@@ -8,7 +8,7 @@
 //  Author        : $Author$
 //  Created By    : $USER_NAME$
 //  Created       : $ASCII_TIME$
-//  Last Modified : <181024.0905>
+//  Last Modified : <181104.0954>
 //
 //  Description	
 //
@@ -41,13 +41,113 @@
 //////////////////////////////////////////////////////////////////////////////
 
 static const char rcsid[] = "@(#) : $Id$";
+#include <Adafruit_NeoPixel.h>
+
+#define NeoPixelPin 6
+
+// Small wheels: 1643: twelve pixels each (2 wheels)
+#define SmallWheels 12
+// Large wheels: 1586: twenty four pixels each (2 wheels)
+#define LargeWheels 24
+// Smoke: 1768: 1/4 60 pixel ring (1)
+#define Smoke 15
+
+
+#define totalPixels ((SmallWheels*2)+(LargeWheels*2)+Smoke)
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(totalPixels,NeoPixelPin, NEO_GRB + NEO_KHZ800);
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+    WheelPos = 255 - WheelPos;
+    if(WheelPos < 85) {
+        return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    }
+    if(WheelPos < 170) {
+        WheelPos -= 85;
+        return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    }
+    WheelPos -= 170;
+    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+#define ThrottlePin A0
+
+#define MAXCYCLE 40
+uint16_t cyclecount = MAXCYCLE;
+uint16_t icycle = MAXCYCLE;
+
+uint8_t  iphase = 0;
+uint8_t  iwheelColor = 0;
+
+void wheelInit(int offset, int wheelsize)
+{
+    for (uint16_t i=offset; i < wheelsize; i+=3) {
+        strip.setPixelColor(i+iphase, Wheel( (i+iwheelColor)  % 255));    //turn every third pixel on
+    }
+}
+void clearWheel(int offset, int wheelsize)
+{
+    for (uint16_t i=offset; i < wheelsize; i+=3) {
+        strip.setPixelColor(i+iphase, 0);
+    }
+}
+
+#define SMOKEOFF ((SmallWheels*2)+(LargeWheels*2))
+#define SMOKELEN 15
+
+void smokeInit()
+{
+    for (uint16_t i=SMOKEOFF; i < SMOKELEN; i+=3) {
+        strip.setPixelColor(i+iphase, strip.Color(127, 127, 127));
+    }
+}
+
+void clearSmoke()
+{
+    for (uint16_t i=SMOKEOFF; i < SMOKELEN; i+=3) {
+        strip.setPixelColor(i+iphase, 0);
+    }
+}
 
 void setup() {
-  // put your setup code here, to run once:
-
+    strip.begin();
+    wheelInit(0,SmallWheels); 
+    wheelInit(SmallWheels,SmallWheels);
+    wheelInit(SmallWheels*2,LargeWheels);
+    wheelInit((SmallWheels*2)+LargeWheels,LargeWheels);
+    smokeInit();
+    strip.show();
+    analogReference(EXTERNAL);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    uint16_t ithrottle = analogRead(ThrottlePin);
+    cyclecount = (int)(((4096-ithrottle)/4096.0)*MAXCYCLE);
+    icycle--;
+    if (icycle == 0) {
+        if (ithrottle > 0) {
+            clearWheel(0,SmallWheels); 
+            clearWheel(SmallWheels,SmallWheels);
+            clearWheel(SmallWheels*2,LargeWheels);
+            clearWheel((SmallWheels*2)+LargeWheels,LargeWheels); 
+        }
+        clearSmoke();
+        strip.show();
+        delay(25);
+        iphase++;
+        if (iphase >= 3) iphase = 0;
+        iwheelColor++;
+        icycle = cyclecount;
+        if (ithrottle > 0) {
+            wheelInit(0,SmallWheels); 
+            wheelInit(SmallWheels,SmallWheels);
+            wheelInit(SmallWheels*2,LargeWheels);
+            wheelInit((SmallWheels*2)+LargeWheels,LargeWheels);
+        }
+        smokeInit();
+        strip.show();
+    }
+    delay(25);
 }
